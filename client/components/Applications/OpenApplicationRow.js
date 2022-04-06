69652697e875adb9cb000167ba5b29fc6a75a17a
Joshua Constine <joshua.constine97@gmail.com>
2022-04-05 22:01:26 -0600

    open applications working

--- client/components/Applications/OpenApplicationRow.js
+++ client/components/Applications/OpenApplicationRow.js
@@ -2,15 +2,22 @@ import React from "react";
 import { Box } from "@mui/material";
 
 const OpenApplicationRow = (props) => {
-  console.log(props.companyName);
   return (
     <Box
       sx={{
         width: "90%",
+        height: 75,
         margin: "5%",
+        backgroundColor: "white",
+        borderRadius: 5,
+        padding: 2,
+        display: "flex",
+        justifyContent: "space-between",
       }}
     >
-      {props.companyName}
+      <Box className="companyInfo theme">{props.companyName}</Box>
+      <Box className="companyInfo theme">{props.jobTitle}</Box>
+      <Box className="companyInfo theme">4/19/2022</Box>
     </Box>
   );
 };
