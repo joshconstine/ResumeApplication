719c8501e6748347857b682fc6fddd0dd229d240
Joshua Constine <joshua.constine97@gmail.com>
2022-04-05 20:30:11 -0600

    created home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,62 @@
-import React from 'react'
-import {connect} from 'react-redux'
-
+import React from "react";
+import { connect } from "react-redux";
+import { Box, Button } from "@mui/material";
+import { useHistory } from "react-router-dom";
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
+export const Home = (props) => {
+  const { username } = props;
+  const history = useHistory();
 
   return (
-    <div>
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <h3>Welcome, {username}</h3>
-    </div>
-  )
-}
+      <Box
+        sx={{
+          width: "90%",
+          height: 500,
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          padding: 5,
+        }}
+        className="theme"
+      >
+        <Box className="homeScreenOption">
+          <Button onClick={() => history.push("/applications")}>
+            Applications
+          </Button>
+        </Box>
+        <Box className="homeScreenOption">
+          <Button onClick={() => history.push("/callander")}>Callander</Button>
+        </Box>
+        <Box className="homeScreenOption">
+          <Button onClick={() => history.push("/tasks")}>Tasks</Button>
+        </Box>
+        <Box className="homeScreenOption">
+          <Button onClick={() => history.push("/templates")}>Templates</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
