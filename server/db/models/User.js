ab354d18d7c6857b88601a500d3aba815ddf7c57
joshua constine <joshua.constine97@gmail.com>
2022-04-12 14:13:54 -0500

    fixed

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -3,7 +3,6 @@ const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const axios = require("axios");
-require("dotenv").config();
 const Application = require("./Application");
 
 const SALT_ROUNDS = 5;
