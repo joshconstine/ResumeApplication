4e7bbb8d076eab8783159c765ef862dbd643785c
joshua constine <joshua.constine97@gmail.com>
2022-04-17 10:09:13 -0500

    sign up working

--- client/components/User/SignUp.js
+++ client/components/User/SignUp.js
@@ -1,5 +1,15 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, connect } from "react-redux";
+import {
+  Container,
+  Box,
+  Typography,
+  Avatar,
+  TextField,
+  Button,
+} from "@mui/material";
+import { LockOutlined } from "@mui/icons-material";
 import { authenticate } from "../../store";
 
 /**
@@ -7,28 +17,98 @@ import { authenticate } from "../../store";
  */
 const SignUp = (props) => {
   const { name, displayName, handleSubmit, error } = props;
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="username">
-            <small>Username</small>
-          </label>
-          <input name="username" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-    </div>
+    <Container>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography variant="h4">SignUp</Typography>
+
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            id="firstName"
+            label="First Name"
+            name="firstName"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            required
+            autoComplete="first name"
+            autoFocus
+            error={!!firstNameError}
+            helperText={firstNameError}
+          />
+
+          <TextField
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            required
+            autoComplete="last name"
+            error={!!lastNameError}
+            helperText={lastNameError}
+          />
+
+          <TextField
+            id="email"
+            label="Email"
+            name="email"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            required
+            autoComplete="email"
+            error={!!emailError}
+            helperText={emailError}
+          />
+
+          <TextField
+            id="password"
+            label="Password"
+            name="password"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            required
+            autoComplete="current-password"
+            type="password"
+            error={!!passwordError}
+            helperText={passwordError}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Link to="/login" variant="body2">
+            Already have an account? Log in
+          </Link>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
@@ -52,10 +132,9 @@ const mapDispatch = (dispatch) => {
   return {
     handleSubmit(evt) {
       evt.preventDefault();
-      const formName = evt.target.name;
-      const username = evt.target.username.value;
+      const username = evt.target.email.value;
       const password = evt.target.password.value;
-      dispatch(authenticate(username, password, formName));
+      dispatch(authenticate(username, password, "signup"));
     },
   };
 };
