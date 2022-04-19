a486d489ebecc624ed71828caa555e2b0a3f8996
joshua constine <joshua.constine97@gmail.com>
2022-04-19 10:29:23 -0500

    added goal input to profile screen

--- client/components/Applications/Goal.js
+++ client/components/Applications/Goal.js
@@ -12,7 +12,7 @@ import { connect, useSelector, useDispatch } from "react-redux";
 
 const Goal = () => {
   let goalRef = useRef("");
-
+  const goal = useSelector((state) => state.auth.goal);
   const dispatch = useDispatch();
 
   async function handleSubmit(e) {
@@ -20,35 +20,37 @@ const Goal = () => {
 
     await dispatch(fetchCreateGoal(goalRef.current.value));
   }
-  const fourms = [{ text: "Goal", ref: goalRef }];
-
+  const fourm = { text: "Goal", ref: goalRef };
+  let hasGoal = true;
+  if (goal === null) hasGoal = false;
   return (
     <Box>
       <Typography variant="h6" className="regFont">
         Job search Goal
       </Typography>
-      <Box className=" regFont column">
-        you currently dont have any goals set!
-      </Box>
-      <Box sx={{ display: "flex" }}>
-        <Box className="column">
-          {fourms.map((fourm, i) => {
-            return (
-              <Box key={i}>
-                <FormControl>
-                  <InputLabel htmlFor="Name">{fourm.text}</InputLabel>
-                  <Input
-                    aria-describedby="my-helper-text"
-                    inputRef={fourm.ref}
-                  />
-                </FormControl>
-              </Box>
-            );
-          })}
+      {!hasGoal ? (
+        <>
+          <Box className=" regFont column">
+            you currently dont have any goals set add a goal below and we will
+            help you acomplish it
+          </Box>
+          <Box className="column">
+            <Box>
+              <FormControl>
+                <InputLabel htmlFor="Name">{fourm.text}</InputLabel>
+                <Input aria-describedby="my-helper-text" inputRef={fourm.ref} />
+              </FormControl>
+            </Box>
 
-          <Button onClick={(e) => handleSubmit(e)}>submit</Button>
+            <Button onClick={(e) => handleSubmit(e)}>submit</Button>
+          </Box>
+        </>
+      ) : (
+        <Box className=" regFont column">
+          current goal {goal} applications per day
         </Box>
-      </Box>
+      )}
+      <Box sx={{ display: "flex" }}></Box>
     </Box>
   );
 };
