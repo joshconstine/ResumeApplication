0d33a552e4164c3153c417ad6eb50a0abe405e3b
joshua constine <joshua.constine97@gmail.com>
2022-05-26 12:08:27 -0700

    test

--- server/api/sms.js
+++ server/api/sms.js
@@ -2,24 +2,24 @@
 // Find your Account SID and Auth Token at twilio.com/console
 
 // and set the environment variables. See http://twil.io/secure
-require("dotenv").config();
-const router = require("express").Router();
-module.exports = router;
-const accountid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require("twilio")(accountid, authToken);
+// require("dotenv").config();
+// const router = require("express").Router();
+// module.exports = router;
+// const accountid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require("twilio")(accountid, authToken);
 
-router.post("/", async (req, res, next) => {
-  try {
-    client.messages
-      .create({
-        body: `this message is to remind you about your applicaiton with ${req.body.companyName} for the position of ${req.body.positionName}`,
-        from: "+16203144588",
-        to: "+19202657335",
-      })
-      .then((message) => console.log("sid", message.sid));
-    res.sendStatus(200);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.post("/", async (req, res, next) => {
+//   try {
+//     client.messages
+//       .create({
+//         body: `this message is to remind you about your applicaiton with ${req.body.companyName} for the position of ${req.body.positionName}`,
+//         from: "+16203144588",
+//         to: "+19202657335",
+//       })
+//       .then((message) => console.log("sid", message.sid));
+//     res.sendStatus(200);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
